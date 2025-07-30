32834be586c4563270784888ef3a54000db8bc49
yeilkk <yeil99@naver.com>
2025-07-30 22:19:42 +0900

    init: project setting

---
+++ src/main/java/com/example/sw_be/SwBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.sw_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SwBeApplication.class, args);
+    }
+
+}
