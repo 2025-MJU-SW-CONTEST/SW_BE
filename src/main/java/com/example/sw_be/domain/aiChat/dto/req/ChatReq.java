74183085770f8ddd53bc7f729599028dbe412fa4
chang min O <82094699+Ochangmin524@users.noreply.github.com>
2025-08-18 19:09:27 +0900

    [FIX] api/chats post 요청으로 수정

--- src/main/java/com/example/sw_be/domain/aiChat/dto/req/ChatReq.java
+++ src/main/java/com/example/sw_be/domain/aiChat/dto/req/ChatReq.java
@@ -1,5 +1,6 @@
 package com.example.sw_be.domain.aiChat.dto.req;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -13,5 +14,22 @@ public class ChatReq {
         private Long movie_id;
         private String text;
         private Long chat_room_id;
+    }
+
+
+    @Data
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class ChatRequestDTO {
+        @Schema(description = "제목 검색 시 상위 k 번째 (0부터 시작합니다)", example = "0", required = false)
+        private Long k;
+        @Schema(description = "검색할 제목 문자열", example = "어벤져스", required = false)
+        private String title;
+        @Schema(description = "제목 조회 이후, 얘기하는 영화 아이디", example = "1", required = false)
+        private Long movieId;
+        @Schema(description = "유저가 보내는 질문", example = "이게 뭐야?", required = false)
+        private String text;
+        @Schema(description = "현재 AI 채팅룸 ID", example = "1", required = true)
+        private Long aiChatRoomId;
     }
 }
