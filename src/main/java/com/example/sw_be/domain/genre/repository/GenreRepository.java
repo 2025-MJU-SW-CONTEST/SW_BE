dd15cba9eada926b1f79403ef472304896553102
rkddpfls02 <rkddpfls02@naver.com>
2025-08-19 14:02:24 +0900

    fix: Analysis  update 수정

--- src/main/java/com/example/sw_be/domain/genre/repository/GenreRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.sw_be.domain.genre.repository;
-
-import com.example.sw_be.domain.genre.entity.Genre;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface GenreRepository extends JpaRepository<Genre, Long> {
-}
